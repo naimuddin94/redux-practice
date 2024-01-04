35268dca87c327c5655b848b08903c9abcdfe16f
naimuddin94 <developernaimuddin@gmail.com>
2024-01-05 01:55:49 +0600

    increment and decrement concept clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-function App() {
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./features/counter/counterSlice";
 
+function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
-  )
+    <section className="w-fit p-10 mx-auto">
+      <h1 className="text-3xl font-bold text-center">Counter: {count}</h1>
+      <div className="flex justify-center gap-3 mt-5">
+        <button
+          className="btn btn-circle btn-accent"
+          onClick={() => dispatch(increment())}
+        >
+          ➕
+        </button>
+        <button
+          className="btn btn-circle btn-secondary"
+          onClick={() => dispatch(decrement())}
+        >
+          ➖
+        </button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
